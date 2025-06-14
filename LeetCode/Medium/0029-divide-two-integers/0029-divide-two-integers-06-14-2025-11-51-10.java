7484e12fcabae2bfb4ac97844bbe5770eb192c40
Pratham singh <spratham845@gmail.com>
2025-06-14 11:51:10 +0530

    1 ms (74.67%) 41.3 MB (24.84%) 06-14-2025 0029-divide-two-integers .java Medium

---
+++ LeetCode/Medium/0029-divide-two-integers/0029-divide-two-integers-06-14-2025-11-51-10.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+
+        long dvd = Math.abs((long)dividend);
+        long dvs = Math.abs((long)divisor);
+        long ans = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs, multiple = 1;
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            ans += multiple;
+        }
+
+        return sign * (int)ans;
+    }
+}
