ba2f7ad1a66224ef386fe1ecd9193dfcdbafaf24
Pratham singh <spratham845@gmail.com>
2025-11-16 10:30:31 +0530

    6 ms (74.5%) 45.5 MB (9.8%) 11-16-2025 0150-evaluate-reverse-polish-notation .java Medium

---
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation-11-16-2025-10-30-30.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+")){
+                stack.push(stack.pop() + stack.pop());
+            }
+            else if(token.equals("-")){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 - num2);
+            }
+            else if(token.equals("*")){
+                stack.push(stack.pop() * stack.pop());
+            }
+            else if(token.equals("/")){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 / num2);
+            }
+            else{
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
