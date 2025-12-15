71033fee0ed3df277c3cb27929cfa68428c0011d
Pratham singh <spratham845@gmail.com>
2025-12-15 10:12:15 +0530

    N/A (0%) N/A (0%) 12-15-2025 0509-fibonacci-number .java Easy

---
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number-12-15-2025-10-12-15.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+       for(int x : n){
+         if(n <= 1){
+            return n;
+        }
+        int last = n - 1;
+        int slast = n - 2;
+        return last + slast;
+       }
+    }
+}
